c2893aef69dca880b066f0594cd39fad479d0ce3
Alvaro Fernandes <fernandes.alvaro@gmail.com>
2020-10-12 20:42:59 +1100

    add browserrouter to fix path problem on gh-pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter, BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Navbar from './Component/navbar';
 import Home from './Component/home';
@@ -10,14 +10,16 @@ import Contact from './Component/contact';
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/About" component={About} />
-        <Route exact path="/Portfolio" component={Portfolio} /> 
-        <Route exact path="/Contact" component={Contact} />
-      </Switch>
+      <BrowserRouter history={history} basename={process.env.PUBLIC_URL}>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/About" component={About} />
+          <Route exact path="/Portfolio" component={Portfolio} /> 
+          <Route exact path="/Contact" component={Contact} />
+        </Switch>
+      </BrowserRouter>
     </Router>
   );
 }

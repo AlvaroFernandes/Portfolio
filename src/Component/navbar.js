eba52dc6179f21e24ef82da0db7c505cd9b54a74
Alvaro Fernandes <fernandes.alvaro@gmail.com>
2020-10-12 20:50:46 +1100

    add {process.env.PUBLIC_URL} on the navbar links

--- src/Component/navbar.js
+++ src/Component/navbar.js
@@ -4,25 +4,25 @@ function Navbar(){
  return(
   <nav className="navbar navbar-expand-lg navbar-light bg-light static-top mb-5 shadow">
   <div className="container">
-    <a className="navbar-brand" href="/home">Alvaro Fernandes</a>
+    <a className="navbar-brand" href={process.env.PUBLIC_URL + "/home"}>Alvaro Fernandes</a>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
     <div className="collapse navbar-collapse" id="navbarResponsive">
       <ul className="navbar-nav ml-auto">
         <li className="nav-item active">
-          <a className="nav-link" href="/home">Home
+          <a className="nav-link" href={process.env.PUBLIC_URL + "/home"}>Home
                 <span className="sr-only">(current)</span>
               </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/About">About</a>
+          <a className="nav-link" href={process.env.PUBLIC_URL + "/About"}>About</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/Portfolio">Portfolio</a>
+          <a className="nav-link" href={process.env.PUBLIC_URL + "/Portfolio"}>Portfolio</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/Contact">Contact</a>
+          <a className="nav-link" href={process.env.PUBLIC_URL + "/Contact"}>Contact</a>
         </li>
       </ul>
     </div>

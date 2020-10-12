c20ce56a94473ffff9d926bff58f788070890d30
Alvaro Fernandes <fernandes.alvaro@gmail.com>
2020-10-12 20:35:38 +1100

    add /home path to navbar

--- src/Component/navbar.js
+++ src/Component/navbar.js
@@ -12,7 +12,7 @@ function Navbar(){
     <div className="collapse navbar-collapse" id="navbarResponsive">
       <ul className="navbar-nav ml-auto">
         <li className="nav-item active">
-          <a className="nav-link" href="/">Home
+          <a className="nav-link" href="/home">Home
                 <span className="sr-only">(current)</span>
               </a>
         </li>
